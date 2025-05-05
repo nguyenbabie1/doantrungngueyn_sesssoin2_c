398d2ac8a6bf43227950f98f09806eebe125a0d1
nguyenbabie1 <brokeniuem569@gmail.com>
2025-05-05 13:20:21 +0700

    Update bai3.cpp

--- bai3.cpp
+++ bai3.cpp
@@ -9,8 +9,8 @@
  	int hieu = num1-num2;
  	printf("hieu cua %d va %d la %d\n", num1,num2,hieu);
  	// tinh tich
- 	int tich = num1*num2;
- 	printf("tich cua %d va %d la%d\n", num1,num2,tich);
+  float tich = (float)num1 * num2;
+  printf("tich cua %d va %d la %.2f\n", num1, num2, tich);
  	// tinh thuong
  	int thuong = num1 / num2;
  	printf("tong cu %d va %d la %d\n",num1, num2, thuong);
